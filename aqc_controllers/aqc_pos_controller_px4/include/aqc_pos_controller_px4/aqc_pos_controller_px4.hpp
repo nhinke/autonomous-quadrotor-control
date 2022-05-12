704576159dbd25fb9a7424ce92170e5a31628284
Nick Hinke <nhinke2@gmail.com>
2022-05-12 12:03:58 -0400

    third commit

--- aqc_controllers/aqc_pos_controller_px4/include/aqc_pos_controller_px4/aqc_pos_controller_px4.hpp
+++ aqc_controllers/aqc_pos_controller_px4/include/aqc_pos_controller_px4/aqc_pos_controller_px4.hpp
@@ -4,12 +4,19 @@
 #include <aqc_msgs/PositionSetpoint.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <mavros_msgs/PositionTarget.h>
+
 #include <dynamic_reconfigure/server.h>
-#include <aqc_pos_controller_px4/aqc_pos_controller_px4Config.h>
+#include <aqc_pos_controller_px4/DrControllerConfig.h>
+
+#include <aqc_msgs/ArmAction.h>
+#include <aqc_msgs/ChangeFlightModeAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+// future note: switch aqc_pos_controller_px4 (package name) and aqc_pos_controller_px4_dr (class and file names)
 
 namespace rsp {
 
-    class aqc_pos_controller_px4 {
+    class aqc_pos_controller_px4_dr {
 
         private:
 
@@ -21,19 +28,26 @@ namespace rsp {
 
             double setpoint_x, setpoint_y, setpoint_z, setpoint_yaw;
 
-            // dynamic_reconfigure::Server<aqc_pos_controller_px4::aqc_pos_controller_px4Config> server;
-            // dynamic_reconfigure::Server<aqc_pos_controller_px4::aqc_pos_controller_px4Config>::CallbackType callback;
-            // dynamic_reconfigure::Server<aqc_pos_controller_px4::ControllerConfig> server;
-            // dynamic_reconfigure::Server<aqc_pos_controller_px4::ControllerConfig>::CallbackType callback;
+            bool first_call_flag;
+            dynamic_reconfigure::Server<aqc_pos_controller_px4::DrControllerConfig> server;
+            dynamic_reconfigure::Server<aqc_pos_controller_px4::DrControllerConfig>::CallbackType callback;
+
+            typedef actionlib::SimpleActionClient<aqc_msgs::ArmAction> ArmClient;
+            typedef actionlib::SimpleActionClient<aqc_msgs::ChangeFlightModeAction> CfmClient;
+            std::unique_ptr<ArmClient> arm_client;
+            std::unique_ptr<CfmClient> cfm_client;
 
         protected:
 
+            void make_arm_request(const int& arm_code);
+            void make_cfm_request(const int& cfm_code);
+            void dr_server_callback(aqc_pos_controller_px4::DrControllerConfig& config, uint32_t level);
             void position_setpoint_callback(const aqc_msgs::PositionSetpoint::ConstPtr& pos_setpoint);
 
         public:
 
-            aqc_pos_controller_px4(ros::NodeHandle& nh);
-            ~aqc_pos_controller_px4();
+            aqc_pos_controller_px4_dr(ros::NodeHandle& nh);
+            ~aqc_pos_controller_px4_dr();
             void publish_setpoint();
 
     };
--- aqc_controllers/aqc_pos_controller_px4/src/aqc_pos_controller_px4.cpp
+++ aqc_controllers/aqc_pos_controller_px4/src/aqc_pos_controller_px4.cpp
@@ -1,20 +1,115 @@
 #include <aqc_pos_controller_px4/aqc_pos_controller_px4.hpp>
 
-rsp::aqc_pos_controller_px4::aqc_pos_controller_px4(ros::NodeHandle& nh) : 
-    nh(nh), setpoint_x(0.0), setpoint_y(0.0), setpoint_z(5.0), setpoint_yaw(0.0) {
+rsp::aqc_pos_controller_px4_dr::aqc_pos_controller_px4_dr(ros::NodeHandle& nh) : 
+    nh(nh), setpoint_x(0.0), setpoint_y(0.0), setpoint_z(5.0), setpoint_yaw(0.0), first_call_flag(true) {
 
-    // pub_twist_setpoint = nh.advertise<geometry_msgs::Twist>("/mavros/setpoint_velocity/cmd_vel_unstamped", 10);
-    // sub_twist_setpoint = nh.subscribe("/cmd_vel", 10, &rsp::aqc_vel_controller_teleop::twist_setpoint_callback, this);
+    arm_client.reset( new ArmClient(nh, "change_arm_status_action") );
+    cfm_client.reset( new CfmClient(nh, "change_flight_mode_action") );
 
-    sub_pos_setpoint = nh.subscribe<aqc_msgs::PositionSetpoint>("/quad/cmd_setpoint", 10, &rsp::aqc_pos_controller_px4::position_setpoint_callback, this);
+    // sub_pos_setpoint = nh.subscribe<aqc_msgs::PositionSetpoint>("/cmd_position", 10, &rsp::aqc_pos_controller_px4_dr::position_setpoint_callback, this);
     pub_pos_setpoint = nh.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local", 10);
     pub_raw_pos_setpoint = nh.advertise<mavros_msgs::PositionTarget>("/mavros/setpoint_raw/local",10);
 
+    callback = boost::bind(&rsp::aqc_pos_controller_px4_dr::dr_server_callback, this, _1, _2);
+    server.setCallback(callback);   
+
+    ros::Duration server_timeout(5);
+    if (!arm_client->waitForServer(server_timeout)) { 
+        ROS_ERROR("'arm_client' within 'aqc_pos_controller_px4' node could not connect to its server. Please ensure that 'aqc_coordinator' node is alive and running.");
+    }
+    if (!cfm_client->waitForServer(server_timeout)) {
+        ROS_ERROR("'cfm_client' within 'aqc_pos_controller_px4' node could not connect to its server. Please ensure that 'aqc_coordinator' node is alive and running.");
+    }
+
 }
 
-rsp::aqc_pos_controller_px4::~aqc_pos_controller_px4() {}
+rsp::aqc_pos_controller_px4_dr::~aqc_pos_controller_px4_dr() {}
 
-void rsp::aqc_pos_controller_px4::position_setpoint_callback(const aqc_msgs::PositionSetpoint::ConstPtr& pos_setpoint) {
+void rsp::aqc_pos_controller_px4_dr::dr_server_callback(aqc_pos_controller_px4::DrControllerConfig& config, uint32_t level) {
+
+    // prevent any changes when node first launched
+    if (first_call_flag) {
+        first_call_flag = false;
+        return;
+    }
+
+    if ((level & 1) == 1) {
+        // std::cout << "\nnew east setpoint: " << (config.east_setpoint) << std::endl;
+        setpoint_x = config.east_setpoint;
+    }
+
+    if ((level & 2) == 2) {
+        // std::cout << "\nnew north setpoint: " << (config.north_setpoint) << std::endl;
+        setpoint_y = config.north_setpoint;
+    }
+
+    if ((level & 4) == 4) {
+        // std::cout << "\nnew up setpoint: " << (config.up_setpoint) << std::endl;
+        setpoint_z = config.up_setpoint;
+    }
+
+    if ((level & 8) == 8) {
+        std::cout << "\nnew yaw setpoint: " << (config.yaw_setpoint) << std::endl;
+        setpoint_yaw = config.yaw_setpoint;
+    }
+
+    if ((level & 16) == 16) {
+        std::cout << "\nnew arm request: " << (config.arm_request) << std::endl;
+        make_arm_request(config.arm_request);
+    }
+
+    if ((level & 32) == 32) {
+        // std::cout << "\nnew cfm request: " << (config.cfm_request) << std::endl;
+        make_cfm_request(config.cfm_request);
+    }
+
+}
+
+void rsp::aqc_pos_controller_px4_dr::make_arm_request(const int& arm_code) {
+
+    aqc_msgs::ArmGoal goal;
+    if (arm_code == 0) {
+        goal.arm_goal.arm_code = goal.arm_goal.DISARM;
+    } else if (arm_code == 1) {
+        goal.arm_goal.arm_code = goal.arm_goal.ARM;
+    } else {
+        goal.arm_goal.arm_code = goal.arm_goal.DISARM;
+    }
+    arm_client->sendGoal(goal);
+
+}
+
+void rsp::aqc_pos_controller_px4_dr::make_cfm_request(const int& cfm_code) {
+
+    aqc_msgs::ChangeFlightModeGoal goal;
+    if (cfm_code == 0) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.TAKEOFF;
+    } else if (cfm_code == 1) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.LAND;
+    } else if (cfm_code == 2) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.RTL;
+    } else if (cfm_code == 3) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.LOITER;
+    } else if (cfm_code == 4) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.OFFBOARD;
+    } else if (cfm_code == 5) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.MANUAL;
+    } else if (cfm_code == 6) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.POSCTL;
+    } else if (cfm_code == 7) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.STABILIZED;
+    } else if (cfm_code == 8) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.ACRO;
+    } else if (cfm_code == 9) {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.ALTCTL;
+    } else {
+        goal.cfm_goal.flight_mode.flight_mode_code = goal.cfm_goal.flight_mode.LOITER;
+    }
+    cfm_client->sendGoal(goal);
+
+}
+
+void rsp::aqc_pos_controller_px4_dr::position_setpoint_callback(const aqc_msgs::PositionSetpoint::ConstPtr& pos_setpoint) {
 
     aqc_msgs::PositionSetpoint setpoint = *pos_setpoint;
     
@@ -25,7 +120,7 @@ void rsp::aqc_pos_controller_px4::position_setpoint_callback(const aqc_msgs::Pos
 
 }
 
-void rsp::aqc_pos_controller_px4::publish_setpoint() {
+void rsp::aqc_pos_controller_px4_dr::publish_setpoint() {
 
     mavros_msgs::PositionTarget msg;
     msg.coordinate_frame = mavros_msgs::PositionTarget::FRAME_LOCAL_NED;
@@ -48,7 +143,7 @@ void rsp::aqc_pos_controller_px4::publish_setpoint() {
 
 }
 
-// void rsp::aqc_pos_controller_px4::publish_setpoint() {
+// void rsp::aqc_pos_controller_px4_dr::publish_setpoint() {
 
 //     geometry_msgs::PoseStamped setpoint;
 //     setpoint.pose.position.x = setpoint_x;
